346c07e05c85e5a83a8b4240b9a375a0673119aa
abacate <rafael.schrott02@gmail.com>
2026-06-05 00:06:37 -0300

    feat: small fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -114,7 +114,7 @@ const config: Config = {
           items: [
             {
               label: 'Introdução',
-              to: '/docs/intro',
+              to: '/docs/rules',
             },
           ],
         },
