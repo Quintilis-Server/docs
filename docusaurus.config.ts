1c620333b41df8ff1df5da9424d0935cd4c37d85
abacate <rafael.schrott02@gmail.com>
2026-06-04 23:44:51 -0300

    feat: nginx added and dockerfile created

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Quintilis',
+  title: 'Quintilis - Documentação',
   tagline: 'Documentação Oficial',
   favicon: 'img/favicon.ico',
 
